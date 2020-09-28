b10889070fc19b1376eef7b38d975c799dede586
Peter D Bethke <pdbethke@siteshell.net>
2020-09-28 10:06:57 -0400

    Version Bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ CLASSIFIERS = []
 
 setup(
     name='ayewa',
-    version=0.01,
+    version=0.02,
     description='Core Module for Ayewa Application',
     author='siteshell.net',
     author_email='pdbethke@siteshell.net',
